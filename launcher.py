4866145ca06dd77943b98d9992eb8233bf110826
Chingis <chingisotchigin@gmail.com>
2021-05-27 20:47:04 +1200

    yeet2

--- launcher.py
+++ launcher.py
@@ -6,5 +6,13 @@ tk = pd.draw(hex_size=30)
 pd[0][1].update_color('blue')
 
 pd[3][7].update_color('red')
+
+
+def correct_quit(tk):
+        tk.destroy()
+        tk.quit()
+
+quit_button = Button(tk, text = "Quit", command = lambda :correct_quit(tk))
+quit_button.pack(pady=10)
 tk.mainloop()
 
